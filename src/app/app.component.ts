30ab709791c73d298972508d53942d42fd274570
Sudhir Bhandari <sudhir.bhandari@cginfinity.com>
2023-06-14 22:05:24 +0530

    chat setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+
+interface Message {
+  sender: string;
+  content: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chat-app';
+  nameInput: string = ""
+  messages: Message[] = [];
+  newMessage: string = "";
+
+  sendMessage(){
+    this.messages.push({"sender": "Sudhir", "content": "How are you"})
+  }
+
+  saveUsername(){
+    localStorage.setItem("userName", this.nameInput)
+  }
+
+  isUserThere(){
+    if(localStorage.getItem("userName")!=""){
+      return true;
+    }
+    return false;
+  }
 }
